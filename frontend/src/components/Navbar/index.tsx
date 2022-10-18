f090c3d367d0bf85f984fbe1aad97ffa088c0280
Paul Anderson <pauldevop@gmail.com>
2022-10-18 15:10:36 -0300

    update github name

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -10,7 +10,7 @@ function Navbar() {
       <a href="https://github.com/paulfms">
         <div className="dsmovie-contact-container">
           <GithubIcon />
-          <p className="dsmovie-contact-link">/devsuperior</p>
+          <p className="dsmovie-contact-link">/paulfms</p>
         </div>
         </a>
     </div>
